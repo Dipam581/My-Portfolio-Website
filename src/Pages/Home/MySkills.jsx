a0f5802babe6b7deab243bfcf915e79d7b068bc9
Dipam Ghosh <107043079+Dipam581@users.noreply.github.com>
2025-06-22 19:39:46 +0530

    Update MySkills.jsx

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -8,17 +8,22 @@ export default function MySkills() {
         <h2 className="skills--section--heading">My Expertise</h2>
       </div>
       <div className="skills--section--container">
-        {data?.skills?.map((item, index) => (
-          <div key={index} className="skills--section--card">
-            <div className="skills--section--img">
-              <img src={item.src} alt="Product Chain" />
+        {data?.skills
+          ?.slice()                                
+          .sort((a, b) => Number(a.id) - Number(b.id))
+          .map((item, index) => (
+            <div key={index} className="skills--section--card">
+              <div className="skills--section--img">
+                <img src={item.src} alt={item.title} />
+              </div>
+              <div className="skills--section--card--content">
+                <h3 className="skills--section--title">{item.title}</h3>
+                <p className="skills--section--description">{item.description}</p>
+              </div>
             </div>
-            <div className="skills--section--card--content">
-              <h3 className="skills--section--title">{item.title}</h3>
-              <p className="skills--section--description">{item.description}</p>
-            </div>
-          </div>
-        ))}
+          ))
+        }
+
       </div>
     </section>
   );
