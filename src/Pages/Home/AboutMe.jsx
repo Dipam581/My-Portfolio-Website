a4489e98dd7fdf1890b63fb43a7629c959db7b9f
Dipam Ghosh <107043079+Dipam581@users.noreply.github.com>
2025-06-22 19:38:21 +0530

    Update AboutMe.jsx

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,10 +9,10 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading" style={{"color":"black"}}>About Me</h1>
           <p className="hero--section-description">
-          As a Software Engineer with nearly 2 years of experience, I specialize in developing scalable solutions using Python, Django, and React.js. I have a solid background in backend development, optimizing server-side processes, and ensuring seamless API integrations. My experience spans full-stack application development, system performance enhancement, and effective team collaboration.
+            I'm a Software Engineer with 2 years of hands-on experience, predominantly in machine learning, AI, and Generative AI. I,ve designed and deployed predictive models and fine-tuned large language models using Python and TensorFlow, built scalable data pipelines for real-time inference, and integrated MLOps best practices to ensure reliability in production. My web development background with Django, Flask serves to streamline data-driven applications, but it’s secondary to my core focus on intelligent systems.
           </p>
           <p className="hero--section-description">
-          I’m passionate about applying my skills in machine learning and modern web technologies to solve complex challenges and deliver impactful results.
+            Passionate about transforming data into actionable insights, I,m dedicated to pushing the boundaries of AI—exploring cutting-edge algorithms, crafting end-to-end RAG architectures, and leveraging cloud-native tools to deliver measurable impact.
           </p>
         </div>
       </div>
